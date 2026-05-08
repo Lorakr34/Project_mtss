022082b8c02dc9ab61a79b9843145e8d202e4f9e
Lorakr34 <gsi16astra0@gmail.com>
2026-05-08 13:26:12 +0200

    Implementata conversione numeri 1-3 e test. Closes #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,5 +5,16 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-    
+    public static String convert(int number){
+        if (number < 1 || number > 3) {
+            return null; 
+        }
+        
+        StringBuilder roman = new StringBuilder();
+        while (number >= 1) {
+            roman.append("I");
+            number = number - 1;
+        }
+        return roman.toString();
+    }
 }
